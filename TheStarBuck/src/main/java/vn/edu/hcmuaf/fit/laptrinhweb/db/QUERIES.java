acc08b250550f49006c8c10d86a25844b8c0e787
KyAnhNguyen <19130011@st.hcmuaf.edu.vn>
2021-12-16 22:15:46 +0700

    add queries
    try add queries

--- TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/db/QUERIES.java
+++ TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/db/QUERIES.java
@@ -1,4 +1,14 @@
 package vn.edu.hcmuaf.fit.laptrinhweb.db;
 
 public class QUERIES {
+//    ACCOUNT
+    public static class ACCOUNT {
+        public static final String GET_LIST = "select * from Account";
+        public static final String CREATE = "insert into Account values (%s, %s, %s, %s, %d, %s, %s, %s, %s, null, null, null, null, null, %d, %s, %s, %s, %s)";
+        public static final String DELETE = "delete from Account where id_ac = %s";
+        public static final String GET_ITEM_BYID = "select * from Account where id_ac = %s";
+        public static final String GET_ITEM_BYNAME = "select * from Account where ac_name = N'%s'";
+    }
+
+//    PRODUCT
 }
