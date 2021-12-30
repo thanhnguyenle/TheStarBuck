c8f8929a9173bcac87a45d1ed2fb0c9b0fe28660
KyAnhNguyen <19130011@st.hcmuaf.edu.vn>
2021-12-30 12:28:37 +0700

    add register
    add register (not yet)

---
+++ TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/controller/web/RegisterServlet.java
@@ -0,0 +1,19 @@
+package vn.edu.hcmuaf.fit.laptrinhweb.controller.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "RegisterServlet", value = "/doRegister")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
