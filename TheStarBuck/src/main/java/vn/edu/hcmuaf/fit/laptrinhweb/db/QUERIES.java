2a9226a7b923410e413317e6bc9f04ef70a4e012
KyAnhNguyen <19130011@st.hcmuaf.edu.vn>
2022-01-05 00:04:33 +0700

    add action
    create account (not yet)

--- TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/db/QUERIES.java
+++ TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/db/QUERIES.java
@@ -11,6 +11,7 @@ public class QUERIES {
         public static final String LOGIN = "select * from Account where ac_username = ? and ac_password = ?";
         public static final String GET_ITEM_BYUSERNAME = "select * from Account where ac_username = ?";
         public static final String GET_ITEM_BYEMAIL = "select * from Account where ac_email = ?";
+        public static final String UPDATE = "update Account set ac_active = ?, ac_lastLogin = ?, ac_groupId = ?, modifiedDate = ?, modifiedBy = ?  where id_ac = ?";
     }
 
 //    PRODUCT
@@ -20,5 +21,14 @@ public static class PRODUCT {
     public static final String DELETE = "delete from Product where id_pr = ?";
     public static final String GET_ITEM_BYID = "select * from Product where id_pr = ?";
     public static final String GET_ITEM_BYNAME = "select * from Product where pr_name = ?";
+    public static final String UPDATE = "update Product set pr_name = ?, pr_csId = ?, pr_ingredients = ?, pr_price = ?, pr_discount = ?, pr_quantity = ?, pr_active = ?, pr_hot = ?, pr_view = ?, pr_description = ?, pr_image = ?, pr_descriptionSeo = ?, pr_keywordSeo = ?, pr_titleSeo = ?, pr_note = ?, modifiedDate = ?, modifiedBy = ? where id_pr = ?";
+}
+
+//CATEGORY
+public static class CATEGORY{
+        public static  final String GET_LIST = "select * from Category";
+        public static  final String CREATE = "insert into Category values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        public static  final String DELETE = "delete from Category where id_cs = ?";
+        public static  final String UPDATE = "update Category set cs_name = ?, cs_icon = ?, cs_avatar = ?, cs_active = ?, modifiedDate = ?, modifiedBy = ? where id_cs = ?";
 }
 }
