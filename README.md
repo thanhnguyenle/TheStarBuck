6352ea7b8bc754157cb9eadf7297dc154aec6208
ThanhDev <19130206@st.hcmuaf.edu.vn>
2022-01-07 15:15:46 +0700

    update

--- README.md
+++
@@ -1,6 +0,0 @@
-# TheStarBuck
-Dự án thiết kế website môn lập trình web - Đại học Nông Lâm
-Sinh viên:
-1. Nguyễn Lê Thành - 19130206
-2. Dương Thị Minh Như - 19130163
-3. Nguyễn Kỳ Anh - 19130117
--- TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/db/impl/DBConnectionPool.java
+++ TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/db/impl/DBConnectionPool.java
@@ -3,30 +3,64 @@ package vn.edu.hcmuaf.fit.laptrinhweb.db.impl;
 import vn.edu.hcmuaf.fit.laptrinhweb.db.IDBConnectionPool;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import static vn.edu.hcmuaf.fit.laptrinhweb.db.DBProperties.*;
-import static vn.edu.hcmuaf.fit.laptrinhweb.db.DBProperties.getDbPassword;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class DBConnectionPool implements IDBConnectionPool {
+    private static final Queue<Connection> availableConnections = new LinkedList<>();
+    private int maxConnection;
 
-    @Override
-    public Connection getConnection() {
-        Connection connection = null;
+    public DBConnectionPool(int maxConnection) {
+        this.maxConnection = maxConnection;
+        initializeConnectionPool();
+    }
+
+    private synchronized void initializeConnectionPool() {
         try {
-            String url;
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            url = "jdbc:mysql://" + getDbHost() + ":" + getDbPort() + "/" + getDbName() + "?useSSL=" + getDbUseSSL();
-            connection = DriverManager.getConnection(url, getDbUsername(), getDbPassword());
-        } catch (SQLException | ClassNotFoundException e) {
+           while (!checkIfConnectionPoolsFull()){
+                Connection newConnection = DBConnectionUtils.openConnection();
+                availableConnections.offer(newConnection);
+           }
+           notifyAll();
+        }catch (Exception e){
             e.printStackTrace();
         }
-        return connection;
+    }
+
+    private boolean checkIfConnectionPoolsFull() {
+        return availableConnections.size() >= maxConnection;
+    }
+
+    @Override
+    public synchronized Connection getConnection() {
+        while(availableConnections.size()==0){
+            //waiting for an existing connection to be freed up
+            try {
+                wait();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        //retrieves and removes the first element of this list
+        return availableConnections.poll();
     }
 
     @Override
     public boolean releaseConnection(Connection connection) {
+        try{
+            if(connection.isClosed()){
+                initializeConnectionPool();
+            }else{
+                //add the specified element as the last element of this list
+                boolean isReleased = availableConnections.offer(connection);
+                //wake up all threads that are waiting for a connection
+                notifyAll();
+                return isReleased;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return false;
     }
 }
