785da7d575708de2064bf5942271bc54e7d41fd8
DuongThiMinhNhu <19130163@st.hcmuaf.edu.vn>
2022-02-09 14:41:48 +0700

    nhu
    nhu

--- TheStarBuck/target/TheStarBuck-1.0.0/template/lib/ckeditor/config.js
+++ TheStarBuck/target/TheStarBuck-1.0.0/template/lib/ckeditor/config.js
@@ -4,10 +4,16 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	config.filebrowserBrowseUrl = '/ckeditor/ckfinder/ckfinder.html';
-	config.filebrowserImageBrowseUrl = '/ckeditor/ckfinder/ckfinder.html?type=Images';
-	config.filebrowserFlashBrowseUrl = '/lib/ckeditor/ckfinder/ckfinder.html?type=Flash';
-	config.filebrowserUploadUrl = '/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
-	config.filebrowserImageUploadUrl = '/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
-	config.filebrowserFlashUploadUrl = '/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
+	config.filebrowserBrowseUrl = 'http://localhost:8080/ckfinder/ckfinder.html';
+
+	config.filebrowserImageBrowseUrl = 'http://localhost:8080/ckfinder/ckfinder.html?type=Images';
+
+	config.filebrowserFlashBrowseUrl = 'http://localhost:8080/ckfinder/ckfinder.html?type=Flash';
+
+	config.filebrowserUploadUrl = 'http://localhost:8080/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+
+	config.filebrowserImageUploadUrl = 'http://localhost:8080/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+
+	config.filebrowserFlashUploadUrl = 'http://localhost:8080/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
+
 };
