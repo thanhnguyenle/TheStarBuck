8bda6416687d75d70f942b8a73bab7b7de108ebc
DuongThiMinhNhu <19130163@st.hcmuaf.edu.vn>
2022-02-18 09:24:46 +0700

    mhu
    nhu

--- TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/controller/web/LoginGoogleServlet.java
+++ TheStarBuck/src/main/java/vn/edu/hcmuaf/fit/laptrinhweb/controller/web/LoginGoogleServlet.java
@@ -33,4 +33,10 @@ public class LoginGoogleServlet extends HttpServlet{
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request,response);
     }
+    // lam vo hieu hoa người dung
+    public static void deleteAvailableSession(HttpServletRequest request) {
+        if (request.getSession(false) != null) {
+            request.getSession(false).invalidate();
+        }
+    }
 }
